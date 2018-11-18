30bb85b2743af076febe5a3b98dc8c19320e43b4
golery <noreply@mail.com>
2018-11-18 14:12:58 -0500

    Add webpack

--- main/src/sandbox/index.js
+++ main/src/sandbox/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Sandbox = () => <div>Hello</div>;
+import App from "./App";
 
 console.log(document.getElementById("app_root"), ReactDOM);
-ReactDOM.render(<Sandbox />, document.getElementById("app_root"));
+
+function render() {
+    ReactDOM.render(<App/>, document.getElementById("app_root"));
+}
+
+render();
+
+
+/**
+ * Re-render for hot module replacement in development.
+ */
+
+if (module.hot) {
+    console.log("Accept App");
+    // module.hot.accept('./App', () => console.log('Reload'))
+    module.hot.accept();
+}
--- main/src/sandbox/webpack.config.js
+++ main/src/sandbox/webpack.config.js
@@ -1,6 +1,22 @@
 const path = require('path');
+const { resolve } = require('path');
+var webpack = require('webpack');
 const config = {
-    entry: __dirname + "/index.js",
+    entry: [
+        'react-hot-loader/patch',
+        //activate HMR for React
+
+        'webpack-dev-server/client?http://localhost:9000',
+        //bundle the client for webpack dev server
+        //and connect to the provided endpoint
+
+        'webpack/hot/only-dev-server',
+        //bundle the client for hot reloading
+        //only- means to only hot reload for successful updates
+
+        "./index.js"
+    ],
+    context: __dirname,
     output: {
         path: __dirname + "/build",
         filename: "index.js",
@@ -53,7 +69,15 @@ const config = {
     devServer: {
         contentBase: __dirname,
         port: 9000,
-    }
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        //activates HMR
+        new webpack.NamedModulesPlugin(),
+        //prints more readable module names in the browser console on HMR updates
+        new webpack.NoEmitOnErrorsPlugin()
+    ]
 };
 
 module.exports = function (env, argv) {
